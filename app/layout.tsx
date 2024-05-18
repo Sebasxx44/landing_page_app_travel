4c327704c29fe512e1206cd9225c2e25254e291f
Sebastian <sanchezz3s47@gmail.com>
2024-05-17 19:27:05 -0500

    landing page

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+
+import Navbar from "@/Components/Navbar";
+import "./globals.css";
+import { Metadata } from "next";
+import Footer from "@/Components/Footer";
+
+export const metadata: Metadata = {
+  title: "Travel App",
+  description: "Travel UI/UX App",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+
+  return (
+    <html lang="en">
+      <body>
+        <Navbar/>
+
+        <main className="relative overflow-hidden">
+          {children}
+        </main>
+        <Footer/>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Camp from "@/Components/Camp";
+import Features from "@/Components/Features";
+import GetApp from "@/Components/GetApp";
+import Guide from "@/Components/Guide";
+import Hero from "@/Components/Hero";
+
+export default function Home() {
+  return (
+
+    <>
+      <Hero/>
+      <Camp/>
+      <Guide/>
+      <Features/>
+      <GetApp/>
+    </>
+   
+  );
+}
